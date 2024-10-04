c6f9340c39167a8c3ccf11c662c5b23be5e4070e
linde12 <lindeoscar@gmail.com>
2024-10-04 18:44:28 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import subprocess
+import shutil
+import os
+
+os.mkdir("testdir")
+subprocess.run(["uv", "venv", ".venv"], cwd="./testdir")
+subprocess.run(["uv", "pip", "install", "markupsafe"], cwd="./testdir")
+shutil.rmtree("testdir")
